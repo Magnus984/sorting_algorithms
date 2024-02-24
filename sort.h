1e9b3c55cd6f078097e6b766487e329f06f969f6
blackdreamer15 <bakijessy@gmail.com>
2024-02-24 04:05:57 +0000

    Update main header file with additional constants, typedef and librairies

--- sort.h
+++ sort.h
@@ -2,6 +2,24 @@
 #define SORT_H
 
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Comparison direction macros for bitonic sort */
+#define UP 0
+#define DOWN 1
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
 
 /**
  * struct listint_s - Doubly linked list node
